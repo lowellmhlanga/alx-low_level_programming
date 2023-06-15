42b563bd9e99e4110511af82469d6f06c4d536ae
Simbarashe Mhlanga <simbarashemhlanga@gmail.com>
2023-06-15 06:23:58 -0700

    Added Scripts and C files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Prints quote to the standard error
  *
- * Return: zero on success
+ * Return: 1 on success
  *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
