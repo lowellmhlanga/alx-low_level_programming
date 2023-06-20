fa1e99a7674ddad56f8f545700e5257bb8abf9cd
Simbarashe Mhlanga <simbarashemhlanga@gmail.com>
2023-06-20 03:05:57 -0700

    Added code

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints lowercase alphabets
+*/
+void print_alphabet(void)
+{
+char xx;
+
+for (xx = 'a' ; xx <= 'z' ; xx++)
+_putchar (xx);
+_putchar ('\n');
+}
