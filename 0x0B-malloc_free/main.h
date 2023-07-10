caa95bad3771e91a9a6cf4441ef04a375f77f28c
Simbarashe Mhlanga <simbarashemhlanga@gmail.com>
2023-07-10 11:10:10 -0700

    Added main file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -53,5 +53,10 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif /* MAIN_H */
